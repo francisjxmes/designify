3a2a15c2032287c664f084c2f75562164c3cc039
francisjxmes <francissalinger25@gmail.com>
2025-12-16 19:10:57 +0000

    Style deliverable upload form with bootstrap

--- orders/forms_deliverable.py
+++ orders/forms_deliverable.py
@@ -1,7 +1,20 @@
 from django import forms
 from .models import Deliverable
 
+
 class DeliverableForm(forms.ModelForm):
     class Meta:
         model = Deliverable
         fields = ["file", "note"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["file"].widget.attrs.update(
+            {"class": "form-control"}
+        )
+        self.fields["note"].widget.attrs.update(
+            {
+                "class": "form-control",
+                "placeholder": "Optional note for the client"
+            }
+        )
