c75b05351cb22577a5646ede4ffc1901be61c6ed
francisjxmes <francissalinger25@gmail.com>
2026-03-31 16:28:54 +0100

    Fix sitemap portfolio URL generation

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -2,8 +2,10 @@ from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 from portfolio.models import PortfolioItem
 
+
 class StaticViewSitemap(Sitemap):
     priority = 0.8
+    changefreq = "weekly"
 
     def items(self):
         return ["home", "portfolio_list", "order_list"]
@@ -11,9 +13,13 @@ class StaticViewSitemap(Sitemap):
     def location(self, item):
         return reverse(item)
 
+
 class PortfolioSitemap(Sitemap):
-    changefreq = "weekly"
     priority = 0.7
+    changefreq = "weekly"
 
     def items(self):
         return PortfolioItem.objects.all()
+
+    def location(self, item):
+        return reverse("portfolio_detail", args=[item.pk])
